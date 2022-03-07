4f9b85bd2100284f814726e5b275387665b501e6
Ahmed-Sadek89 <81380165+Ahmed-Sadek89@users.noreply.github.com>
2022-03-07 16:31:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import {render} from 'react-dom';
-//import { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import './Styles/Css/Index.css';
 import { Provider } from 'react-redux';
 import Store from './Redux/Store';
-//import Loading from './General/Loading/Loading';
-import App from './App';
-//const App = lazy(() => import('./App'));
+import Loading from './General/Loading/Loading';
+//import App from './App';
+const App = lazy(() => import('./App'));
 
 render(
-        <Provider store={Store}>
-            <App />
-        </Provider>,
+        <Suspense fallback={Loading}>
+                <Provider store={Store}>
+                    <App />
+                </Provider>
+        </Suspense>
+                ,
     document.getElementById('root')
 )
